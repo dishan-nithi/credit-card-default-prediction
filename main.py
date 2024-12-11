0746ebe0e05aabf0b236667926b2e6762ce37674
dishan-nithi <dishan.nithi@gmail.com>
2024-12-11 14:18:51 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.default_predictor import logger
+from default_predictor.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
-logger.info("Welcome")
+STAGE_NAME = 'DATA INGESTION'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> STAGE {STAGE_NAME} STARTED <<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>> STAGE {STAGE_NAME} COMPLETED <<<<<\n\n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
